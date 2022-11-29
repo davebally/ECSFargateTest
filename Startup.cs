c397a41bdfcf6e2d6783ba036f577014aecb8e94
Dave Ballantyne <d.ballantyne@sportradar.com>
2022-11-29 10:03:26 +0000

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECSFargateTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ECSFargateTest", Version = "v1" });
+            });
+
+            services.AddDefaultAWSOptions(s => s.GetService<IConfiguration>().GetAWSOptions());
+
+            // Makes the Amazon CloudWatch SDK available in the DI container
+            //services.AddAWSService<IAmazonCloudWatch>();
+            //services.AddScoped<CloudWatchClient>();
+
+            // Confifures the ASP.NET Core logging to write logs to the log group provided in your CDK code
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddAWSProvider();
+                loggingBuilder.SetMinimumLevel(LogLevel.Debug);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+         //   if (env.IsDevelopment())
+           // {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECSFargateTest v1"));
+        //    }
+
+           /// app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
